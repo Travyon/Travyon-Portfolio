0b8fb0f6cbc1b1c05a92b8737905c2d5d66746bd
Travyon <treystvns05@gmail.com>
2023-05-26 00:24:02 -0500

    refresh project

---
+++ my-app/src/components/SignIn.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import { Button, Container } from '@mui/material';
+
+const pages = ['Products', 'Pricing', 'Blog']
+
+function SignIn(){
+
+
+  return (
+    <AppBar>
+
+    </AppBar>
+    )
+
+   
+}
+
+export default SignIn;
