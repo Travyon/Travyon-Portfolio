d9dd10f67bcbb31c5addff71fd75ba375913b563
Travyon <treystvns05@gmail.com>
2023-05-31 17:35:07 -0500

    navbar added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import SignIn from './components/SignIn';
+import { Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import React from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <SignIn/>
-    </div>
+      <div className="App">
+        <Header/>
+        {/* <Router>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<x/>}/>
+          <Route path="/education" element={<y/>}/>
+          <Route path="/technicalskills" element={<z/>}/>
+          <Route path="/experience" element={<NotFound/>}/>
+          <Route path="/projects" element={<null/>}/>
+          <Route path="/professionalleadership" element={<null/>}/>
+        </Router> */}
+      </div>
+   
   );
 }
 
---
+++ my-app/src/Theme.js
@@ -0,0 +1,9 @@
+import createTheme  from '@mui/material/styles/createTheme';
+import { amber, blue, blueGrey } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: blue,
+  }
+})
+export default theme
