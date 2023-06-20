687f392e958faec434ef2ba8bee65fec0f73b11a
Travyon <treystvns05@gmail.com>
2023-06-19 21:19:16 -0500

    Hover fixed and new text fields are being developed

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -1,6 +1,46 @@
 import React from "react";
+import { Box, Container, Typography, colors } from "@mui/material";
+import { TextField } from "@mui/material";
+import { styled } from "@mui/material"
 
-const Home = () =>{
+
+const StyledTextField = styled(TextField)(() => ({
+    
+
+}));
+const Home = () => {
+
+    return (
+
+        <Container>
+            <Box>
+                <Typography
+                    variant="h5"
+                    noWrap
+                    sx={{
+                        color: 'white',
+                        textAlign: 'center',
+                        marginTop: '200px',
+                        fontFamily: 'Avenir'
+                    }}
+                >
+                    Create Your Account
+                </Typography>
+            </Box>
+            <div>
+            </div>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+            </Box>
+        </Container>
+
+    )
 
 }
- export default Home;
+export default Home;
